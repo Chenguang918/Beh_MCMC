50aef7bb325bc4adc25fd3b45e902a067780c41d
Chenguang918 <chenguang@bnu.edu.cn>
2022-04-09 23:33:21 +0800

    initial1

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# 这是一个示例 Python 脚本。
+# Visualized MCMC-behavior data
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+class LoadData():
+
+    def LoadCsvData(self, filename):
+        with open(filename) as raw_data:
+            data = pd.read_csv(raw_data)
+        return data
+
+class PlotViolin():
+
+    def PlotCombinedViolin(self, xlabel, ylabel, combine_label, data): # self 方法内的实例化调用的前提条件
+        #
+
+        sns.set(style='whitegrid', color_codes=True)
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+        sns.violinplot(x=xlabel, y=ylabel, hue=combine_label, data=data, split=True, ax=axes[0], inner='point',alpha=0.5)
+        plt.show()
 
 
 # 按间距中的绿色按钮以运行脚本。
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-# test
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    data1 = LoadData()  # 实例化
+    plot1 = PlotViolin()
+    data=data1.LoadCsvData('C:/Users/lgh/OneDrive/Paper5/MCMC_data/text.csv') # 反斜杠才对
+    plot1.PlotCombinedViolin('Exp','Kmax','tPBM',data)
